46639c8ab7642e09beb2a58af7dabcf4b40df777
aygzs123 <1090707415@qq.com>
2022-02-12 13:13:23 +0800

    完成了登录和注册功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -14,7 +14,6 @@ $(function() {
     // 从 layui 中获取 form 对象
     var form = layui.form
     var layer = layui.layer
-        // 通过 form.verify() 函数自定义校验规则
     form.verify({
         // 自定义了一个叫做 pwd 校验规则
         pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
@@ -33,24 +32,23 @@ $(function() {
 
     // 监听注册表单的提交事件
     $('#form_reg').on('submit', function(e) {
-        // 1. 阻止默认的提交行为
-        e.preventDefault()
-            // 2. 发起Ajax的POST请求
-        var data = {
-            username: $('#form_reg [name=username]').val(),
-            password: $('#form_reg [name=password]').val()
-        }
-        $.post('/api/reguser', data, function(res) {
-            if (res.status !== 0) {
-                return layer.msg(res.message)
+            // 1. 阻止默认的提交行为
+            e.preventDefault()
+                // 2. 发起Ajax的POST请求
+            var data = {
+                username: $('#form_reg [name=username]').val(),
+                password: $('#form_reg [name=password]').val()
             }
-            layer.msg('注册成功，请登录！')
-                // 模拟人的点击行为
-            $('#link_login').click()
+            $.post('/api/reguser', data, function(res) {
+                if (res.status != 0) {
+                    return layer.msg(res.message, { icon: 5 })
+                }
+                layer.msg('注册成功,请登录!', { icon: 6 })
+                    // 模拟人的点击行为
+                $('#link_login').click()
+            })
         })
-    })
-
-    // 监听登录表单的提交事件
+        // 监听登录表单的提交事件
     $('#form_login').submit(function(e) {
         // 阻止默认提交行为
         e.preventDefault()
@@ -61,9 +59,9 @@ $(function() {
             data: $(this).serialize(),
             success: function(res) {
                 if (res.status !== 0) {
-                    return layer.msg('登录失败！')
+                    return layer.msg('登录失败!', { icon: 5 })
                 }
-                layer.msg('登录成功！')
+                layer.msg('登录成功!', { icon: 6 })
                     // 将登录成功得到的 token 字符串，保存到 localStorage 中
                 localStorage.setItem('token', res.token)
                     // 跳转到后台主页
